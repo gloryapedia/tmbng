f0021ef21f1789acbcae1e4f376731381a6d9fef
gloryapedia <85855318+gloryapedia@users.noreply.github.com>
2021-08-13 00:14:57 +0700

    Add files via upload

--- BLConfig.py
+++ BLConfig.py
@@ -533,6 +533,501 @@ Manual  ={
                            "Max": 6.6
                            }
                 },{
+                "Comment"       : "1",
+                "Status"        : "on",
+                "Base Bet"      : 0.025,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 66,
+                           "Max": 66
+                           }
+                },{
+                "Comment"       : "2",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 56.5,
+                           "Max": 56.5
+                           }
+                },{
+				"Comment"       : "3",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 49.5,
+                           "Max": 49.5
+                           }
+                },{
+                "Comment"       : "4",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 44,
+                           "Max": 44
+                           }
+                },{
+				"Comment"       : "5",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 39.6,
+                           "Max": 39.6
+                           }
+                },{
+				"Comment"       : "6",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 36,
+                           "Max": 36
+                           }
+                },{
+				"Comment"       : "7",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 33,
+                           "Max": 33
+                           }
+                },{
+                "Comment"       : "8",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 30.4,
+                           "Max": 30.4
+                           }
+                },{
+				"Comment"       : "9",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 28.2,
+                           "Max": 28.2
+                           }
+                },{
+                "Comment"       : "10",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 26.4,
+                           "Max": 26.4
+                           }
+                },{
+				"Comment"       : "11",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 24.75,
+                           "Max": 24.75
+                           }
+                },{
+				"Comment"       : "12",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 23.2,
+                           "Max": 23.2
+                           }
+                },{
+				"Comment"       : "13",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 22,
+                           "Max": 22,
+                           }
+                },{
+                "Comment"       : "14",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 20.8,
+                           "Max": 20.8
+                           }
+                },{
+                "Comment"       : "15",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 19.8,
+                           "Max": 19.8
+                           }
+                },{
+                "Comment"       : "16",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 18.85,
+                           "Max": 18.85
+                           }
+                   },{
+                "Comment"       : "17",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 18,
+                           "Max": 18
+                           }
+                },{
+                "Comment"       : "18",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 17.2,
+                           "Max": 17.2
+                           }
+                  },{
+                "Comment"       : "19",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 16.5,
+                           "Max": 16.5
+                           }
+                   },{
+                "Comment"       : "20",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 15.84,
+                           "Max": 15.84
+                           }
+                  },{
+                "Comment"       : "21",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 15.2,
+                           "Max": 15.2
+                           }
+                  },{
+                "Comment"       : "22",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 14.6,
+                           "Max": 14.6
+                           }
+                 },{
+                "Comment"       : "23",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 14.1,
+                           "Max": 14.1
+                           }
+                 },{
+                "Comment"       : "24",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 13.6,
+                           "Max": 13.6
+                          }
+                 },{
+                "Comment"       : "25",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 13.2,
+                           "Max": 13.2
+                          }
+                 },{
+                "Comment"       : "26",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 12.7,
+                           "Max": 12.7
+                           }
+                 },{
+                "Comment"       : "27",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 12.37,
+                           "Max": 12.37
+                           }
+                  },{
+                "Comment"       : "28",
+                "Status"        : "on",
+                "Base Bet"      : 0.0003,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 12,
+                           "Max": 12
+                           }
+                  },{
+                "Comment"       : "29",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 11.64,
+                           "Max": 11.64
+                           }
+                 },{
+                "Comment"       : "30",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 11.31,
+                           "Max": 11.31
+                           }
+                 },{
+                "Comment"       : "31",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 11,
+                           "Max": 11
+                           }
+                  },{
+                "Comment"       : "32",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 10.7,
+                           "Max": 10.7
+                           }
+                  },{
+                  "Comment"       : "33",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 10.4,
+                           "Max": 10.4
+                           }
+                },{
+				"Comment"       : "34",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 10.1,
+                           "Max": 10.1
+                           }
+                },{
+				"Comment"       : "35",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 9.9,
+                           "Max": 9.9
+                           }
+                },{
+				"Comment"       : "36",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 9.65,
+                           "Max": 9.65
+                           }
+                },{
+				"Comment"       : "37",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 9.42,
+                           "Max": 9.42
+                           }
+                },{
+				"Comment"       : "38",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 9.20,
+                           "Max": 9.20
+                           }
+                },{
+				"Comment"       : "39",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 9,
+                           "Max": 9,
+                           }
+                },{
+                "Comment"       : "40",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 8.8,
+                           "Max": 8.8
+                           }
+                },{
+				"Comment"       : "41",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 8.60,
+                           "Max": 8.60
+                           }
+                },{
+				"Comment"       : "42",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 8.42,
+                           "Max": 8.42
+                           }
+                },{
+				"Comment"       : "43",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 8.25,
+                           "Max": 8.25
+                           }
+                },{
+				"Comment"       : "44",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 8,
+                           "Max": 8
+                           }
+                },{
+				"Comment"       : "45",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 7.9,
+                           "Max": 7.9
+                           }
+                },{
+				"Comment"       : "46",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 7.7,
+                           "Max": 7.7
+                           }
+                },{
+				"Comment"       : "47",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 7.6,
+                           "Max": 7.6
+                           }
+                },{
+                "Comment"       : "48",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 7.4,
+                           "Max": 7.4
+                           }
+                },{
+				"Comment"       : "49",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 7.3,
+                           "Max": 7.3
+                           }
+                },{
+				"Comment"       : "50",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 7.2,
+                           "Max": 7.2
+                           }
+                },{
+				"Comment"       : "51",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 7,
+                           "Max": 7
+                           }
+                },{
+				"Comment"       : "52",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 6.94,
+                           "Max": 6.94
+                           }
+                },{
+				"Comment"       : "53",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 6.82,
+                           "Max": 6.82
+                           }
+                },{
+				"Comment"       : "54",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 6.7,
+                           "Max": 6.7
+                           }
+                },{
+				"Comment"       : "55",
+                "Status"        : "on",
+                "Base Bet"      : 0.00002,
+                "Repeat Bets"   : 1,
+                "Chance"  :{
+                           "Min": 6.6,
+                           "Max": 6.6
+                           }
+                },{
                 "Comment"       : "56",
                 "Status"        : "on",
                 "Base Bet"      : 0.0221,
